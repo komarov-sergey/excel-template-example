e8f0703d8535d4b744c06f29a76402fb43c77370
Sergey Komarov <komarovs33@mail.ru>
2024-08-06 14:19:01 +0300

    feat: clear

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const R = require("ramda");
+const { getData, fillTemplate, wrileToFile } = require("./helpers");
 
-const sequelize = require("./db/db.service");
-const { processData, prepareData } = require("./helpers");
-
-sequelize
-  .query(`select id, type, "DealerName" from public."Dealers" limit 3`)
-  .then((result) => prepareData(R.head(result)))
-  .then((result2) => processData("./templates/ruamds1.xlsx", result2))
-  .then((result3) => result3.workbook().toFileAsync("./out/ruamds1_out.xlsx"));
+getData()
+  .then(fillTemplate)
+  .then(wrileToFile)
+  .catch((e) => console.log(e));
